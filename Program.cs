1737196df73460c0fdab43b7f2083090b64adb94
CesarRojas2019-8652 <78991630+CesarRojas2019-8652@users.noreply.github.com>
2021-03-04 21:34:26 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿
+//programa que prepare espacio para un máximo de 100 nombres. El usuario deberá ir introduciendo un nombre cada vez, hasta que se pulse Intro sin teclear nada, momento en el que dejarán de pedirse más nombres y se mostrará en pantalla la lista de los nombres que se han introducido.
 using System;
 
 
