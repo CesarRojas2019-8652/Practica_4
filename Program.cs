ef7b8ffa811222c3ae83bfd8d9bfd30f51a35f0c
CesarRojas2019-8652 <78991630+CesarRojas2019-8652@users.noreply.github.com>
2021-03-04 21:33:46 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+
+
+namespace practica_04_ejercicio_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Nombres = new string[101];
+            Console.WriteLine("Escriba los nombres de la lista, para concluir presione enter estando en blanco");
+            int i = -1;
+
+            do
+            {
+                i++;
+                Nombres[i] = Console.ReadLine();
+            }
+             while (Nombres[i].Length != 0);
+
+            
+            Console.Clear();
+            Console.WriteLine("Nombres ingresados: ");
+            i = -1;
+
+            do
+            {
+                i++;
+                Console.WriteLine(Nombres[i]);
+            } while (Nombres[i].Length != 0);
+
+            Console.ReadKey();
+        }
+
+
+
+            }
+        }
+    
+
