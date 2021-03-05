e2ecd63012ee7eef094e088d548d4efecb28f9ed
CesarRojas2019-8652 <78991630+CesarRojas2019-8652@users.noreply.github.com>
2021-03-04 20:51:00 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿//programa que pida al usuario 5 números reales(pista: necesitarás un array de "float") y luego los muestre en el orden contrario al que se introdujeron.
+using System;
+
+
+namespace practica_4_ejercicio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float[] Numeros = new float[5];
+            Console.Clear();
+            for (int i = 0; i <= 4; i++)
+            {
+                int M = 1 + i;
+                Console.WriteLine("Ingrese un numero {0} ", M);
+                Numeros[i] = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+            }
+  
+            for (int i = 4; i >= 0; i--)
+            {
+                int M = 1 + i;
+                Console.WriteLine(" El numero: " + M + "[" + Numeros[i] + "] ");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
