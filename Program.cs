c76f007ee2d4fb7070d0842fed384b4ed8ed95b4
CesarRojas2019-8652 <78991630+CesarRojas2019-8652@users.noreply.github.com>
2021-03-04 20:17:12 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace Practica_04_ejercicio_3 
+{
+    class Program
+    {
+        
+            public static void Main()
+            {
+                Console.WriteLine("programa que diga los dias que tiene cada mes y lo muestre en pantalla");
+            int[]
+             meses = { 31,28,31,30,31,30,31,31,30,31,30,31};
+            string[] Mes =
+                {
+                "Enero",
+                "Febrero",
+                "Marzo",
+                "Abri",
+                "Mayo",
+                "Junio",
+                "Julio",
+                "Agosto",
+                "Septiembre",
+                "Octubre",
+                "Noviembre",
+                "Diciembre"
+            };
+            Console.Clear();
+
+            int M = 0;
+            Console.WriteLine("coloque el numero del mes osea que el 1 es igual a enero");
+            M = Convert.ToInt32(Console.ReadLine());
+            int SE = M - 1;
+            Console.WriteLine("El mes que seleccionó es: {0}\n Y este mes posee: {1} dias\n ", Mes[SE], meses[SE]);
+            Console.ReadKey();
+        }
+    }
+    }
+
+    
+
